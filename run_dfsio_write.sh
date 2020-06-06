06e40e0d5b0c68a54961bd484d373c751bc13ec4
Omar Campos Rivera <megathodik@gmail.com>
2020-06-06 08:52:52 -0400

    Changing TestDFIO Write parameters to 100 files, 1 GB each

--- run_dfsio_write.sh
+++ run_dfsio_write.sh
@@ -2,8 +2,8 @@
 
 hadoop jar $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.10.0-tests.jar TestDFSIO \
         -write \
-        -nrFiles 15 \
-        -size 60GB \
+        -nrFiles 100 \
+        -size 1GB \
         -resFile ~/ocampos/hadoop-baremetal-test/results/TestDFSIOResults.txt
 
 hdfs dfs -rm -r -f /benchmarks
